f6cea4eb29580e861b2179f373ce9572011f0a32
RenhuaChi <renhuachi@tencent.com>
2017-08-30 19:34:22 +0800

    Initial Commit

---
+++ CoreML/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreML
+//
+//  Created by Renhuachi on 2017/8/30.
+//  Copyright © 2017年 tencent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreML/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreML
+//
+//  Created by Renhuachi on 2017/8/30.
+//  Copyright © 2017年 tencent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
